79e75f30f28690ce765f1227e00d8661f5ca7ee9
Thiti Dev <thiti.mwk.main@gmail.com>
2021-03-17 23:37:55 +0700

    update: Created (Class)ConfigManagement with -> () => <extractConfigVars> (0.0.1)

---
+++ src/utilities/conf_management/core.ts
@@ -0,0 +1,16 @@
+import * as config from 'config';
+
+const isObject = (thing: any) => typeof thing === 'object' && thing !== null;
+
+export default class ConfigManagement {
+  private static configDataDictionary = {};
+
+  public static extractConfigVariables(ymlHead: string): any {
+    if (!this.configDataDictionary.hasOwnProperty(ymlHead)) {
+      const prefabData = config.get(ymlHead);
+      if (!isObject(prefabData)) return {}; // return empty object because the extracted value is null/undef (is not an object)
+      Object.assign(this.configDataDictionary, { [ymlHead]: prefabData });
+    }
+    return this.configDataDictionary[ymlHead];
+  }
+}
