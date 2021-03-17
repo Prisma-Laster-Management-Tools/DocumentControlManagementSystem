cbb54957ec690eefe4720fdc8b1193f613c95f7e
Thiti Dev <thiti.mwk.main@gmail.com>
2021-03-17 23:44:04 +0700

    update: applied custom written -> conf_management to the main entry

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as config from 'config';
+import { Logger } from '@nestjs/common';
+import ConfigManagement from './utilities/conf_management';
 
 async function bootstrap() {
+  const serverConfig = ConfigManagement.extractConfigVariables('server');
+  const logger = new Logger('bootstrap');
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const port = process.env.PORT || serverConfig.port;
+  await app.listen(port, () =>
+    logger.log(`Application listening on port ${port}`),
+  );
 }
 bootstrap();
