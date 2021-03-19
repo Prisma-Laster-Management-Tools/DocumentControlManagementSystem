825805d432ccd9a447acd0132ec468341a9b6a33
Thiti Dev <thiti.mwk.main@gmail.com>
2021-03-19 23:39:57 +0700

    update: (fn)created signIn -> auth.service

--- src/authentication/authentication.service.ts
+++ src/authentication/authentication.service.ts
@@ -1,4 +1,30 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { InjectRepository } from '@nestjs/typeorm';
+import { SignUpCredentialsDto } from 'src/user/dto/signup-credentials.dto';
+import { UserRepository } from 'src/user/user.repository';
+import { JwtPayload } from './strategies/jwt.payload';
 
 @Injectable()
-export class AuthenticationService {}
+export class AuthenticationService {
+  constructor(
+    @InjectRepository(UserRepository) private userRepository: UserRepository,
+    private jwtService: JwtService,
+  ) {}
+
+  async signIn(
+    signUpCredentialsDto: SignUpCredentialsDto,
+  ): Promise<{ accessToken: string }> {
+    const user = await this.userRepository.validateUserPassword(
+      signUpCredentialsDto,
+    );
+    if (!user) {
+      throw new UnauthorizedException('Invalid credentials');
+    }
+
+    const payload: JwtPayload = { email: user.email };
+    const accessToken = this.jwtService.sign(payload);
+
+    return { accessToken };
+  }
+}
