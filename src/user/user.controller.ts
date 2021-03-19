0db8858e603f8cddcc734b386705c0698ee24084
Thiti Dev <thiti.mwk.main@gmail.com>
2021-03-19 23:57:12 +0700

    update: Created (test-only) <getmydata> api path

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -5,11 +5,15 @@ import {
   Get,
   HttpCode,
   Post,
+  Req,
+  UseGuards,
   ValidationPipe,
 } from '@nestjs/common';
 import { UserService } from './user.service';
 import { SignUpCredentialsDto } from './dto/signup-credentials.dto';
 import { AuthenticationService } from 'src/authentication/authentication.service';
+import { AuthGuard } from '@nestjs/passport';
+import { GetUser } from 'src/shared/decorators/get-user.decorator';
 
 @Controller('user')
 export class UserController {
@@ -32,5 +36,12 @@ export class UserController {
     @Body(ValidationPipe) authCredentialsDto: SignUpCredentialsDto,
   ): Promise<{ accessToken: string }> {
     return this.authenticationService.signIn(authCredentialsDto);
+  }
+
+  // This method would be available throughout the development phase only [WILL BE REMOVED later]
+  @Get('/getmydata')
+  @UseGuards(AuthGuard())
+  getMyData(@GetUser() user) {
+    return user;
   }
 }
