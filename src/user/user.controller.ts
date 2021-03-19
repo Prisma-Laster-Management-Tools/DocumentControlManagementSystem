77ba139c6638372739ab105fed7702700a95f9e0
Thiti Dev <thiti.mwk.main@gmail.com>
2021-03-19 23:42:25 +0700

    update: finalized login api

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -8,10 +8,14 @@ import {
 } from '@nestjs/common';
 import { UserService } from './user.service';
 import { SignUpCredentialsDto } from './dto/signup-credentials.dto';
+import { AuthenticationService } from 'src/authentication/authentication.service';
 
 @Controller('user')
 export class UserController {
-  constructor(private userService: UserService) {}
+  constructor(
+    private userService: UserService,
+    private authenticationService: AuthenticationService,
+  ) {}
 
   @Post('registration')
   signUp(
@@ -19,5 +23,12 @@ export class UserController {
     signUpCredentialDto: SignUpCredentialsDto,
   ) {
     return this.userService.signUp(signUpCredentialDto);
+  }
+
+  @Post('/login')
+  signIn(
+    @Body(ValidationPipe) authCredentialsDto: SignUpCredentialsDto,
+  ): Promise<{ accessToken: string }> {
+    return this.authenticationService.signIn(authCredentialsDto);
   }
 }
