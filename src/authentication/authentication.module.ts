802252e9b98551f1cfc324bce92d53f2af0043fe
Thiti Dev <thiti.mwk.main@gmail.com>
2021-03-19 23:38:49 +0700

    update: applied jwt&passport modules&services to authModule

--- src/authentication/authentication.module.ts
+++ src/authentication/authentication.module.ts
@@ -1,7 +1,16 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserRepository } from 'src/user/user.repository';
 import { AuthenticationService } from './authentication.service';
+import {
+  modules_importation,
+  JWTSExportationModules,
+  JwtStrategy,
+} from './strategies/jwt.strategy';
 
 @Module({
-  providers: [AuthenticationService]
+  imports: [...modules_importation, TypeOrmModule.forFeature([UserRepository])],
+  providers: [AuthenticationService, JwtStrategy],
+  exports: [...JWTSExportationModules], // passport module
 })
 export class AuthenticationModule {}
