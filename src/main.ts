7ff2a811d8e2c391a18accdff23cd9c02c24ac57
Thiti Dev <thiti.mwk.main@gmail.com>
2021-04-10 01:23:17 +0700

    Applied express-session middleware by invoke created (fn)applyMiddleware

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,14 @@ import * as config from 'config';
 import { BadRequestException, Logger, ValidationPipe } from '@nestjs/common';
 import ConfigManagement from './utilities/conf_management';
 import { ValidationPipeWithCustomException } from './validation';
+import { applyMiddlewares } from './app.middleware';
 
 async function bootstrap() {
   const serverConfig = ConfigManagement.extractConfigVariables('server');
   const logger = new Logger('bootstrap');
   const app = await NestFactory.create(AppModule);
+  applyMiddlewares(app);
+
   app.useGlobalPipes(ValidationPipeWithCustomException);
   app.setGlobalPrefix('api'); // prefix the route with /api/
 
