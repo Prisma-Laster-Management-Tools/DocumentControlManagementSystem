6b71bd20803d18ab024efcb3afc5fc0435bd8d14
Thiti Dev <thiti.mwk.main@gmail.com>
2021-04-16 00:56:01 +0700

    Used authGuard,positionsGuard in feedback controller, settle meta-deta of position => 'purchasement' for whole controller

--- src/feedback/feedback.controller.ts
+++ src/feedback/feedback.controller.ts
@@ -6,14 +6,20 @@ import {
   NotAcceptableException,
   Post,
   Session,
+  UseGuards,
 } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
 import { ISessionItems } from 'src/@types/session';
+import { Positions } from 'src/shared/guards/position/positions.decorator';
+import { PositionsGuard } from 'src/shared/guards/position/positions.guard';
 import { SESSION_ExtractDataAndClear } from 'src/shared/helpers/Session';
 import { CreateFeedbackDTO } from './dto/create-feedback.dto';
 import { FeedbackService } from './feedback.service';
 import { Feedback } from './model/feedback.entity';
 
 @Controller('feedback')
+@UseGuards(AuthGuard(), PositionsGuard)
+@Positions('purchasement') // Only user with purchasement role can access this [ TESTING ]
 export class FeedbackController {
   constructor(private feedbackService: FeedbackService) {}
   @Get()
