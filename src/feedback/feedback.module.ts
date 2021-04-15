a9e590384903b0871c7e8f33d45f9b5e89c37a56
Thiti Dev <thiti.mwk.main@gmail.com>
2021-04-16 00:54:24 +0700

    imported authenticationModule in feedback => for usage of the authGuard decorator

--- src/feedback/feedback.module.ts
+++ src/feedback/feedback.module.ts
@@ -1,5 +1,7 @@
 import { Module } from '@nestjs/common';
+import { PassportModule } from '@nestjs/passport';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthenticationModule } from 'src/authentication/authentication.module';
 import { SalesRepository } from 'src/sales/sales.repository';
 import { SalesService } from 'src/sales/sales.service';
 import { FeedbackController } from './feedback.controller';
@@ -7,7 +9,10 @@ import { FeedbackRepository } from './feedback.repository';
 import { FeedbackService } from './feedback.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([FeedbackRepository, SalesRepository])],
+  imports: [
+    TypeOrmModule.forFeature([FeedbackRepository, SalesRepository]),
+    AuthenticationModule, // needed because if we want to use the AuthGurad decorator from passport module!!!
+  ],
   controllers: [FeedbackController],
   providers: [FeedbackService, SalesService],
 })
