f753172f7ef1065bf62843c47ddcd5288d4eff64
Thiti Dev <thiti.mwk.main@gmail.com>
2021-04-16 17:43:44 +0700

    imported authentication module inside notification module

--- src/notification/notification.module.ts
+++ src/notification/notification.module.ts
@@ -1,11 +1,15 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthenticationModule } from 'src/authentication/authentication.module';
 import { NotificationController } from './notification.controller';
 import { NotificationRepository } from './notification.repository';
 import { NotificationService } from './notification.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([NotificationRepository])],
+  imports: [
+    TypeOrmModule.forFeature([NotificationRepository]),
+    AuthenticationModule,
+  ],
   controllers: [NotificationController],
   providers: [NotificationService],
 })
