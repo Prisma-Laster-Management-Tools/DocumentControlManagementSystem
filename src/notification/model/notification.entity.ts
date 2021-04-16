56f4a0aa62f2024658ccb4e25e086814c86ce65f
Thiti Dev <thiti.mwk.main@gmail.com>
2021-04-16 17:24:11 +0700

    created notification model entity

---
+++ src/notification/model/notification.entity.ts
@@ -0,0 +1,31 @@
+import {
+  BaseEntity,
+  Column,
+  CreateDateColumn,
+  Entity,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+  Unique,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity()
+export class Notification extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ nullable: true }) // can be null -> null means no restriction
+  related_positions: string; // [multiple position are allow] ->
+
+  @Column()
+  message: string; // any string can be embedded to this column
+
+  @Column()
+  attached_params: string; // should be separating with [,] comma likes -> the first splited one would be pointing to the id of something then the [1] one should performs some operation
+
+  @CreateDateColumn({ type: 'timestamp' })
+  createdAt: Date;
+
+  @UpdateDateColumn({ type: 'timestamp' })
+  updatedAt: Date;
+}
