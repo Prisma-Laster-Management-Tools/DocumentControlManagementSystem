af436ad592391ff6dc252e7132b11c13cbdd8cff
Thiti Dev <thiti.mwk.main@gmail.com>
2021-04-26 00:53:37 +0700

    fixed wrong spelling by mistake

--- src/purchasement/model/purchasement-source.entity.ts
+++ src/purchasement/model/purchasement-source.entity.ts
@@ -1,7 +1,7 @@
 import { BaseEntity, Column, CreateDateColumn, Entity, JoinColumn, ManyToOne, OneToOne, PrimaryColumn, PrimaryGeneratedColumn, Unique, UpdateDateColumn } from 'typeorm';
 
 @Entity()
-export class PurchansementSource extends BaseEntity {
+export class PurchasementSource extends BaseEntity {
   @PrimaryGeneratedColumn()
   id: number;
 
--- src/purchasement/purchasement-source.repository.ts
+++ src/purchasement/purchasement-source.repository.ts
@@ -3,8 +3,8 @@ import { Sales } from 'src/sales/model/sales.entity';
 import { ResponseMsg } from 'src/shared/helpers/ResponseMsg';
 
 import { EntityRepository, Repository } from 'typeorm';
-import { PurchansementSource } from './model/purchasement-source.entity';
-@EntityRepository(PurchansementSource)
-export class PurchansementSourceRepository extends Repository<PurchansementSource> {
+import { PurchasementSource } from './model/purchasement-source.entity';
+@EntityRepository(PurchasementSource)
+export class PurchansementSourceRepository extends Repository<PurchasementSource> {
   private logger = new Logger();
 }
