465e08a48519bec7080c37931da3e6b56297b1f1
Thiti Dev <thiti.mwk.main@gmail.com>
2021-04-26 00:48:29 +0700

    created purchasement-source model entity

---
+++ src/purchasement/model/purchasement-source.entity.ts
@@ -0,0 +1,25 @@
+import { BaseEntity, Column, CreateDateColumn, Entity, JoinColumn, ManyToOne, OneToOne, PrimaryColumn, PrimaryGeneratedColumn, Unique, UpdateDateColumn } from 'typeorm';
+
+@Entity()
+export class PurchansementSource extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  part_number: string;
+
+  @Column()
+  company: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  seller: string; // people who sells this part to our company
+
+  @CreateDateColumn({ type: 'timestamp' })
+  createdAt: Date;
+
+  @UpdateDateColumn({ type: 'timestamp' })
+  updatedAt: Date;
+}
