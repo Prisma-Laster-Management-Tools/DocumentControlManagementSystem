da9d55fb2cb6e777578742c2002d62bd9fc3b8fe
Thiti Dev <thiti.mwk.main@gmail.com>
2021-04-29 00:56:05 +0700

    imported all typeorm-feature within qc module

--- src/quality-control/quality-control.module.ts
+++ src/quality-control/quality-control.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { QualityControlProtocolRepository } from './quality-control-protocal.repository';
 import { QualityControlController } from './quality-control.controller';
+import { QualityControlRepository } from './quality-control.repository';
 import { QualityControlService } from './quality-control.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([QualityControlRepository, QualityControlProtocolRepository])],
   controllers: [QualityControlController],
-  providers: [QualityControlService]
+  providers: [QualityControlService],
 })
 export class QualityControlModule {}
