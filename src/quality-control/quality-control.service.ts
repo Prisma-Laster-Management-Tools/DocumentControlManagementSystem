2dcee7f7d39bcc99e27e8f45a84d75d65a4bb2db
Thiti Dev <thiti.mwk.main@gmail.com>
2021-04-29 00:55:51 +0700

    qc -> connected linked repos together within 1 obj

--- src/quality-control/quality-control.service.ts
+++ src/quality-control/quality-control.service.ts
@@ -1,4 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { QualityControlProtocolRepository } from './quality-control-protocal.repository';
+import { QualityControlRepository } from './quality-control.repository';
+
+interface ILinkedRepositories {
+  protocol: QualityControlProtocolRepository;
+  product: QualityControlRepository;
+}
 
 @Injectable()
-export class QualityControlService {}
+export class QualityControlService {
+  private linked_repositories: Partial<ILinkedRepositories> = {};
+  constructor(
+    @InjectRepository(QualityControlProtocolRepository) private qualityControlProtocolRepository: QualityControlProtocolRepository,
+    @InjectRepository(QualityControlRepository) private qualityControlRepository: QualityControlRepository,
+  ) {
+    this.linked_repositories.product = qualityControlRepository;
+    this.linked_repositories.protocol = qualityControlProtocolRepository;
+  }
+}
