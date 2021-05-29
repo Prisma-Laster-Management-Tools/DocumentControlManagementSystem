d8cfdee68873f68be3581ab350dda1275816f7ea
Thiti Dev <thiti.mwk.main@gmail.com>
2021-05-29 23:17:02 +0700

    created qc-q-repos -> and init in the module file

--- src/quality-control/quality-control.module.ts
+++ src/quality-control/quality-control.module.ts
@@ -4,12 +4,13 @@ import { ProductModule } from 'src/product/product.module';
 import { ProductRepository } from 'src/product/product.repository';
 import { ProductService } from 'src/product/product.service';
 import { QualityControlProtocolRepository } from './quality-control-protocal.repository';
+import { QualityControlQueueRepository } from './quality-control-queue.repository';
 import { QualityControlController } from './quality-control.controller';
 import { QualityControlRepository } from './quality-control.repository';
 import { QualityControlService } from './quality-control.service';
 
 @Module({
-  imports: [ProductModule, TypeOrmModule.forFeature([QualityControlRepository, QualityControlProtocolRepository])],
+  imports: [ProductModule, TypeOrmModule.forFeature([QualityControlRepository, QualityControlProtocolRepository, QualityControlQueueRepository])],
   controllers: [QualityControlController],
   providers: [QualityControlService],
 })
