bd8aca1da4798ea3900eaf028f46d1ead14ac9a2
Thiti Dev <thiti.mwk.main@gmail.com>
2021-06-06 01:02:19 +0700

    finalized returned format of (fns)uploadSinglePhoto,uploadMultiplePhoto

--- src/utilities/fs/image-upload/index.ts
+++ src/utilities/fs/image-upload/index.ts
@@ -1,25 +1,32 @@
 import * as fs from 'fs';
-const PUBLIC_FOLDER = 'uploads/';
+const PUBLIC_FOLDER = 'public/uploads/';
 
 interface IUploadResponse {
   success: boolean;
   errors?: any;
+  stored_path?: Array<string> | string;
 }
 
 export async function uploadSinglePhoto(file: Express.Multer.File): Promise<IUploadResponse> {
+  const new_file_name = Date.now().toString() + '_' + file.originalname;
+  const relative_path = PUBLIC_FOLDER + new_file_name;
   return new Promise((resolve, reject) => {
-    fs.writeFile(PUBLIC_FOLDER + file.originalname, file.buffer, (err) => {
+    fs.writeFile(relative_path, file.buffer, (err) => {
       if (err) return resolve({ success: false, errors: err });
-      console.log('file saved to ', file.originalname);
-      resolve({ success: true });
+      console.log('file saved as ', new_file_name);
+      resolve({ success: true, stored_path: relative_path.replace('public/', '') });
     });
   });
 }
 
 export function uploadMultiplePhoto(files: Array<Express.Multer.File>): IUploadResponse {
+  const stored_path = [];
   files.forEach((file) => {
-    fs.writeFileSync(PUBLIC_FOLDER + file.originalname, file.buffer);
+    const new_file_name = Date.now().toString() + '_' + file.originalname;
+    const relative_path = PUBLIC_FOLDER + new_file_name;
+    fs.writeFileSync(relative_path, file.buffer);
+    stored_path.push(relative_path.replace('public/', ''));
   });
   console.log(`total ${files.length} files were uploaded`);
-  return { success: true };
+  return { success: true, stored_path };
 }
