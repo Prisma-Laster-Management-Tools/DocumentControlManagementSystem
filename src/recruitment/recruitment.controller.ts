5feb07363737922b4b0e6d0dde8391306d42e004
Thiti Dev <thiti.mwk.main@gmail.com>
2021-06-06 22:09:34 +0700

    created (api)getAllRecruitments

--- src/recruitment/recruitment.controller.ts
+++ src/recruitment/recruitment.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { RecruitmentService } from './recruitment.service';
 
 @Controller('recruitment')
-export class RecruitmentController {}
+export class RecruitmentController {
+  constructor(private recruitmentService: RecruitmentService) {}
+
+  @Get()
+  getAllRecruitments() {
+    return this.recruitmentService.getAllRecruitments();
+  }
+}
--- src/recruitment/recruitment.service.ts
+++ src/recruitment/recruitment.service.ts
@@ -1,4 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { RecruitmentRepository } from './recruitment.repository';
 
 @Injectable()
-export class RecruitmentService {}
+export class RecruitmentService {
+  constructor(@InjectRepository(RecruitmentRepository) private recruitmentRepository: RecruitmentRepository) {}
+  async getAllRecruitments() {
+    return await this.recruitmentRepository.find();
+  }
+}
