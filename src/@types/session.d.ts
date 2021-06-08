05484ab814c94cec9c76d215a5afc511d78bca12
Thiti Dev <thiti.mwk.main@gmail.com>
2021-06-09 00:48:54 +0700

    added common property for declaration of the session obj

--- src/@types/session.d.ts
+++ src/@types/session.d.ts
@@ -3,6 +3,8 @@ import '@nestjs/common';
 interface ISessionItems {
   arbitary_data?: any;
   param_cached?: any;
+  save: (fn: Function) => any;
+  cookie: any;
 }
 
 declare module '@nestjs/common' {
