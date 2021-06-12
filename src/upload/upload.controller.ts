49b750d5dbdbb128eea3248ca8b3127d84e3e5e0
Thiti Dev <thiti.mwk.main@gmail.com>
2021-06-12 22:10:20 +0700

    created (api)uploadFile -> upload module

--- src/upload/upload.controller.ts
+++ src/upload/upload.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Controller, InternalServerErrorException, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { uploadSinglePhoto } from 'src/utilities/fs/image-upload';
+import { UploadService } from './upload.service';
 
 @Controller('upload')
-export class UploadController {}
+export class UploadController {
+  constructor(private uploadService: UploadService) {}
+
+  @Post('image-single')
+  @UseInterceptors(FileInterceptor('file'))
+  async uploadFile(@UploadedFile() file: Express.Multer.File) {
+    const upload = await uploadSinglePhoto(file);
+    if (!upload.success) {
+      throw new InternalServerErrorException(`Failed to upload the image`);
+    }
+    return { ...upload };
+  }
+}
