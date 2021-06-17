a3399b6d923f951f060ef0ffd1ef3fb13ab2ca1c
Thiti Dev <thiti.mwk.main@gmail.com>
2021-06-18 02:45:53 +0700

    implemented NotificationGateway -> (fn)sendNotificationMessage

--- src/notification.gateway.ts
+++ src/notification.gateway.ts
@@ -1,14 +1,33 @@
 import { Injectable } from '@nestjs/common';
-import { WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
+import { MessageBody, OnGatewayConnection, OnGatewayInit, SubscribeMessage, WebSocketGateway, WebSocketServer, WsResponse } from '@nestjs/websockets';
+import { Server } from 'socket.io';
 
-@WebSocketGateway(80, { namespace: 'notification' })
+@WebSocketGateway(80, { namespace: '/notification' })
 //@Injectable()
-export class NotificationGateway {
+export class NotificationGateway implements OnGatewayInit, OnGatewayConnection {
+  handleConnection(client: any, ...args: any[]) {
+    console.log('got connection: ' + client);
+  }
   @WebSocketServer()
-  server; // to be broadcasting to all people later on ...
+  wss: Server; // to be broadcasting to all people later on ...
+
+  afterInit(server: any) {
+    console.log('[SOCKET-IO]: has been initialized');
+  }
 
   // Instance method to be used from other module/service/controller that made this injected
   test() {
     console.log('works!');
+  }
+
+  @SubscribeMessage('events')
+  handleEvent(@MessageBody() data: unknown): WsResponse<unknown> {
+    const event = 'events';
+    //this.wss.emit('events', data);
+    return { event, data };
+  }
+
+  sendNotificationMessage(data: any) {
+    return this.wss.emit('notification', data);
   }
 }
